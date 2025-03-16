ccbb55467e9e398204ad1fc29854db6d39578018
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-03-16 17:49:11 +0900

    [Silver IV] Title: 로프, Time: 112 ms, Memory: 37048 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,16 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-rope = []
-for _ in range(n):
-  rope.append(int(input()))
+N = int(input())
+arr = sorted([int(input()) for _ in range(N)])
 
-rope.sort()
+result = 0
+for i in range(len(arr)):
+    weight = arr[i] * (len(arr)-i)
+    result = max(result, weight)
 
-result = []
+print(result)
 
-for i in rope:
-  result.append(i * n)
-  n -= 1
-print(max(result))
