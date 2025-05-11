f193592e67ba0d5c4a748cb57c886607f22f4670
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-05-11 16:40:43 +0900

    [Silver I] Title: 봄버맨, Time: 684 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -1,36 +1,33 @@
-import sys
-
-input = sys.stdin.readline
-
-def boom(x, y):
-    temp[x][y] = '.'
+def solve(i, j):
+    temp[i][j] = '.'
 
     for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
+        ni = i + di[k]
+        nj = j + dj[k]
 
-        if 0 <= nx < r and 0 <= ny < c and temp[nx][ny] != '.':
-            temp[nx][ny] = '.'
+        if 0<=ni<R and 0<=nj<C:
+            temp[ni][nj] = '.'
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-r, c, n = map(int, input().split())
-board = [list(input().strip()) for _ in range(r)]
+R, C, N = map(int, input().split())
+board = [list(input()) for _ in range(R)]
 time = 1
 
-if n % 2 == 0:
-    for _ in range(r):
-        print('O' * c)
-else:
-    while time < n:
-        temp = [['O'] * c for _ in range(r)]
-        for i in range(r):
-            for j in range(c):
-                if board[i][j] == 'O':
-                    boom(i, j)
-        else:
-            board = temp
-            time += 2
-    for row in board:
-        print(''.join(row))
+di = [-1,1,0,0]
+dj = [0,0,-1,1]
+
+if N % 2 == 0:
+    for _ in range(R):
+        print('O' * C)
+    exit(0)
+
+while time < N:
+    temp = [['O'] * C for _ in range(R)]
+    for i in range(R):
+        for j in range(C):
+            if board[i][j] == 'O':
+                solve(i, j)
+    board = temp
+    time += 2
+
+for b in board:
+    print(''.join(b))
