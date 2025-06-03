c64a117a2410f8eb7cad4d21392cef394d910d7c
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-06-03 16:44:32 +0900

    [Silver III] Title: 퇴사, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -4,6 +4,5 @@ schedule = [list(map(int, input().split())) for _ in range(N)]
 dp = [0] * (N+1)
 for i in range(N):
     for j in range(i+schedule[i][0], N+1):
-        if dp[j] < dp[i] + schedule[i][1]:
-            dp[j] = dp[i] + schedule[i][1]
+        dp[j] = max(dp[j], dp[i] + schedule[i][1])
 print(dp[-1])
