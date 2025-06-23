7c229beb1db3fea48cd44a3b6442d4e25720780d
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-06-23 20:49:54 +0900

    [level 1] Title: 모의고사, Time: 3.83 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,23 +3,20 @@ def solution(answers):
     s2 = [2,1,2,3,2,4,2,5]
     s3 = [3,3,1,1,2,2,4,4,5,5]
     result = []
+    c = [0,0,0]
     
-    c1, c2, c3 = 0, 0, 0
     for i in range(len(answers)):
         if answers[i] == s1[i % len(s1)]:
-            c1 += 1
+            c[0] += 1
         if answers[i] == s2[i % len(s2)]:
-            c2 += 1
+            c[1] += 1
         if answers[i] == s3[i % len(s3)]:
-            c3 += 1
-    mx = max(c1,c2,c3)
+            c[2] += 1
     
-    if mx == c1:
-        result.append(1)
-    if mx == c2:
-        result.append(2)
-    if mx == c3:
-        result.append(3)
+    for idx, s in enumerate(c):
+        if s == max(c):
+            result.append(idx+1)
+
     return result
         
             
