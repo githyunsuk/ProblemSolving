02af0d00eb1dd0d339d20ed82c7a9db5ec88a62a
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-09-02 06:18:05 +0900

    [Bronze III] Title: 최댓값, Time: 108 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,27 +1,26 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		Scanner scanner = new Scanner(System.in);
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
 		
-		int[] array = new int[9];
-		for(int i=0; i<9; i++)
-			array[i] = scanner.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int max = 0;
-		int index = 0;
+		int num = -1;
 		
-		for(int i=0; i<9; i++)
-		{
-			if(array[i] > max)
-			{
-				max = array[i];
-				index = i+1;
+		for(int i=0; i<9; i++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n > max) {
+				max = n;
+				num = i+1;
 			}
 		}
 		
-		System.out.println(max + "\n" + index);
+		System.out.println(max);
+		System.out.println(num);
 	}
 }
