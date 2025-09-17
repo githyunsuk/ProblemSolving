5bca52f943ae42722de5ae3fdf4d78d85d6e22ea
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-09-17 23:08:11 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String num = br.readLine();
+		int[] arr = new int[10];
+		
+		for(int i=0; i<num.length(); i++) {
+			arr[num.charAt(i) - '0']++;
+		}
+		
+		int temp = (int) Math.ceil((arr[6] + arr[9]) / 2.0);
+		arr[6] = temp;
+		arr[9] = temp;
+		
+		
+		
+		int max = 1;
+		for(int i : arr) {
+			max = Math.max(i, max);
+		}
+		
+		System.out.println(max);
+	}
+}
