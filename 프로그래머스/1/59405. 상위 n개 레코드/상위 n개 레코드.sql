855d25cb06e92134f913ed5ebd7a02223ebb4b37
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-12-08 21:44:29 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-select name
-from (select name 
-      from animal_ins
-      order by datetime)
-where rownum = 1
+SELECT NAME
+FROM (SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME)
+WHERE ROWNUM <= 1
